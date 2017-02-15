fa270ba0133157648e78418943179ed35786e5a6
Alistair Johnson <alistairewj@gmail.com>
2017-02-15 16:19:58 -0500

    added uo, renamed bg and lab vars

--- queries/data.sql
+++ queries/data.sql
@@ -22,58 +22,62 @@ select
   , gcs.EndoTrachFlag
   -- blood gases
   -- oxygen related parameters
-  , bg.SO2
-  , bg.PO2, bg.PCO2
+  , bg.SO2 as bg_SO2
+  , bg.PO2 as bg_PO2
+  , bg.PCO2 as bg_PCO2
   -- also calculate AADO2
-  -- , bg.AADO2
+  -- , bg.AADO2 as bg_AADO2
   --, AADO2_calc
-  , bg.PaO2FiO2Ratio
+  , bg.PaO2FiO2Ratio as bg_PaO2FiO2Ratio
   -- acid-base parameters
-  , bg.PH
-  , bg.BASEEXCESS
-  , bg.BICARBONATE
-  , bg.TOTALCO2
+  , bg.PH as bg_PH
+  , bg.BASEEXCESS as bg_BASEEXCESS
+  , bg.BICARBONATE as bg_BICARBONATE
+  , bg.TOTALCO2 as bg_TOTALCO2
 
   -- blood count parameters
-  , bg.HEMATOCRIT
-  , bg.HEMOGLOBIN
-  , bg.CARBOXYHEMOGLOBIN
-  , bg.METHEMOGLOBIN
+  , bg.HEMATOCRIT as bg_HEMATOCRIT
+  , bg.HEMOGLOBIN as bg_HEMOGLOBIN
+  , bg.CARBOXYHEMOGLOBIN as bg_CARBOXYHEMOGLOBIN
+  , bg.METHEMOGLOBIN as bg_METHEMOGLOBIN
 
   -- chemistry
-  , bg.CHLORIDE
-  , bg.CALCIUM
-  , bg.TEMPERATURE
-  , bg.POTASSIUM
-  , bg.SODIUM
-  , bg.LACTATE
-  , bg.GLUCOSE
+  , bg.CHLORIDE as bg_CHLORIDE
+  , bg.CALCIUM as bg_CALCIUM
+  , bg.TEMPERATURE as bg_TEMPERATURE
+  , bg.POTASSIUM as bg_POTASSIUM
+  , bg.SODIUM as bg_SODIUM
+  , bg.LACTATE as bg_LACTATE
+  , bg.GLUCOSE as bg_GLUCOSE
 
   -- ventilation stuff that's sometimes input
   -- , INTUBATED, TIDALVOLUME, VENTILATIONRATE, VENTILATOR
-  -- , bg.PEEP, O2Flow
+  -- , bg.PEEP as bg_PEEP
+  -- , O2Flow
   -- , REQUIREDO2
 
   -- labs
-  , lab.ANIONGAP as ANIONGAP_lab
-  , lab.ALBUMIN as ALBUMIN_lab
-  , lab.BANDS as BANDS_lab
-  , lab.BICARBONATE as BICARBONATE_lab
-  , lab.BILIRUBIN as BILIRUBIN_lab
-  , lab.CREATININE as CREATININE_lab
-  , lab.CHLORIDE as CHLORIDE_lab
-  , lab.GLUCOSE as GLUCOSE_lab
-  , lab.HEMATOCRIT as HEMATOCRIT_lab
-  , lab.HEMOGLOBIN as HEMOGLOBIN_lab
-  , lab.LACTATE as LACTATE_lab
-  , lab.PLATELET as PLATELET_lab
-  , lab.POTASSIUM as POTASSIUM_lab
-  , lab.PTT as PTT_lab
-  , lab.INR as INR_lab
-  , lab.PT as PT_lab
-  , lab.SODIUM as SODIUM_lab
-  , lab.BUN as BUN_lab
-  , lab.WBC as WBC_lab
+  , lab.ANIONGAP as ANIONGAP
+  , lab.ALBUMIN as ALBUMIN
+  , lab.BANDS as BANDS
+  , lab.BICARBONATE as BICARBONATE
+  , lab.BILIRUBIN as BILIRUBIN
+  , lab.CREATININE as CREATININE
+  , lab.CHLORIDE as CHLORIDE
+  , lab.GLUCOSE as GLUCOSE
+  , lab.HEMATOCRIT as HEMATOCRIT
+  , lab.HEMOGLOBIN as HEMOGLOBIN
+  , lab.LACTATE as LACTATE
+  , lab.PLATELET as PLATELET
+  , lab.POTASSIUM as POTASSIUM
+  , lab.PTT as PTT
+  , lab.INR as INR
+  , lab.PT as PT
+  , lab.SODIUM as SODIUM
+  , lab.BUN as BUN
+  , lab.WBC as WBC
+
+  , uo.UrineOutput
 from mp_hourly_cohort mp
 left join mp_vital vi
   on  mp.icustay_id = vi.icustay_id
@@ -81,6 +85,9 @@ left join mp_vital vi
 left join mp_gcs gcs
   on  mp.icustay_id = gcs.icustay_id
   and mp.hr = gcs.hr
+left join mp_uo uo
+  on  mp.icustay_id = uo.icustay_id
+  and mp.hr = uo.hr
 left join mp_bg_art bg
   on  mp.hadm_id = bg.hadm_id
   and mp.hr = bg.hr
