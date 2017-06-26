6b1b4ce4319fa94e0aea218393b997a6131a448d
Alistair Johnson <alistairewj@gmail.com>
2017-06-26 18:02:30 -0400

    merge blood gases into approximate equivalents in labs/charted data

--- queries/data.sql
+++ queries/data.sql
@@ -11,44 +11,35 @@ select
   , vi.DiasBP
   , vi.MeanBP
   , vi.RespRate
-  , vi.TempC
-  , vi.SpO2
-  , vi.Glucose as glucose_chart
+  , coalesce(bg.TEMPERATURE, vi.TempC) as tempc
+  , coalesce(bg.SO2, vi.SpO2) as spo2
+  , coalesce(lab.GLUCOSE,bg.GLUCOSE,vi.Glucose) as glucose
+
   -- gcs
   , gcs.GCS
   , gcs.GCSMotor
   , gcs.GCSVerbal
   , gcs.GCSEyes
   , gcs.EndoTrachFlag
+
   -- blood gases
   -- oxygen related parameters
-  , bg.SO2 as bg_SO2
   , bg.PO2 as bg_PO2
   , bg.PCO2 as bg_PCO2
+
   -- also calculate AADO2
   -- , bg.AADO2 as bg_AADO2
   --, AADO2_calc
   , bg.PaO2FiO2Ratio as bg_PaO2FiO2Ratio
+
   -- acid-base parameters
   , bg.PH as bg_PH
   , bg.BASEEXCESS as bg_BASEEXCESS
-  , bg.BICARBONATE as bg_BICARBONATE
   , bg.TOTALCO2 as bg_TOTALCO2
 
   -- blood count parameters
-  , bg.HEMATOCRIT as bg_HEMATOCRIT
-  , bg.HEMOGLOBIN as bg_HEMOGLOBIN
   , bg.CARBOXYHEMOGLOBIN as bg_CARBOXYHEMOGLOBIN
   , bg.METHEMOGLOBIN as bg_METHEMOGLOBIN
-
-  -- chemistry
-  , bg.CHLORIDE as bg_CHLORIDE
-  , bg.CALCIUM as bg_CALCIUM
-  , bg.TEMPERATURE as bg_TEMPERATURE
-  , bg.POTASSIUM as bg_POTASSIUM
-  , bg.SODIUM as bg_SODIUM
-  , bg.LACTATE as bg_LACTATE
-  , bg.GLUCOSE as bg_GLUCOSE
 
   -- ventilation stuff that's sometimes input
   -- , INTUBATED, TIDALVOLUME, VENTILATIONRATE, VENTILATOR
@@ -60,20 +51,20 @@ select
   , lab.ANIONGAP as ANIONGAP
   , lab.ALBUMIN as ALBUMIN
   , lab.BANDS as BANDS
-  , lab.BICARBONATE as BICARBONATE
+  , coalesce(lab.BICARBONATE,bg.BICARBONATE) as BICARBONATE
   , lab.BILIRUBIN as BILIRUBIN
+  , bg.CALCIUM as CALCIUM
   , lab.CREATININE as CREATININE
-  , lab.CHLORIDE as CHLORIDE
-  , lab.GLUCOSE as GLUCOSE
-  , lab.HEMATOCRIT as HEMATOCRIT
-  , lab.HEMOGLOBIN as HEMOGLOBIN
-  , lab.LACTATE as LACTATE
+  , coalesce(lab.CHLORIDE, bg.CHLORIDE) as CHLORIDE
+  , coalesce(lab.HEMATOCRIT,bg.HEMATOCRIT) as HEMATOCRIT
+  , coalesce(lab.HEMOGLOBIN,bg.HEMOGLOBIN) as HEMOGLOBIN
+  , coalesce(lab.LACTATE,bg.LACTATE) as LACTATE
   , lab.PLATELET as PLATELET
-  , lab.POTASSIUM as POTASSIUM
+  , coalesce(lab.POTASSIUM, bg.POTASSIUM) as POTASSIUM
   , lab.PTT as PTT
   , lab.INR as INR
-  , lab.PT as PT
-  , lab.SODIUM as SODIUM
+  -- , lab.PT as PT -- PT and INR are redundant
+  , coalesce(lab.SODIUM, bg.SODIUM) as SODIUM
   , lab.BUN as BUN
   , lab.WBC as WBC
 
