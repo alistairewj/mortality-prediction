8f321b5ff7c73eaaea79c61cc879507daa2280e7
Alistair Johnson <alistairewj@gmail.com>
2017-10-08 15:34:46 -0400

    update to use mimic-code views

--- queries/data.sql
+++ queries/data.sql
@@ -1,10 +1,15 @@
--- this combines all views to get all features at all time points
---
+-- FINAL DATA TABLE!
+-- This combines (1) the base cohort with (2) materialized views to get patient data
+-- The result is a table which is (N*Hn)xM
+--  Rows: N patients times Hn hours for each patient (hours is variable)
+--  Columns: M features
+-- the "hr" column is the integer hour since ICU admission
+-- it can be negative since some labs are measured before ICU admission
 DROP TABLE IF EXISTS mp_data CASCADE;
 CREATE TABLE mp_data as
 select
   mp.subject_id, mp.hadm_id, mp.icustay_id
-  , mp.hr
+  , ih.hr
   -- vitals
   , vi.HeartRate
   , vi.SysBP
@@ -69,20 +74,31 @@ select
   , lab.WBC as WBC
 
   , uo.UrineOutput
-from mp_hourly_cohort mp
-left join mp_vital vi
-  on  mp.icustay_id = vi.icustay_id
-  and mp.hr = vi.hr
-left join mp_gcs gcs
-  on  mp.icustay_id = gcs.icustay_id
-  and mp.hr = gcs.hr
-left join mp_uo uo
-  on  mp.icustay_id = uo.icustay_id
-  and mp.hr = uo.hr
-left join mp_bg_art bg
+-- source from our "base" cohort
+from mp_cohort mp
+-- add in every hour for their icu stay
+inner join icustay_hours ih
+  on mp.icustay_id = ih.icustay_id
+-- now left join to all the data tables using the hours
+left join pivoted_vital vi
+  on  ih.icustay_id = vi.icustay_id
+  and ih.endtime - interval '1' hour < vi.charttime
+  and ih.endtime >= vi.charttime
+left join pivoted_gcs gcs
+  on  ih.icustay_id = gcs.icustay_id
+  and ih.endtime - interval '1' hour < gcs.charttime
+  and ih.endtime >= gcs.charttime
+left join pivoted_uo uo
+  on  ih.icustay_id = uo.icustay_id
+  and ih.endtime - interval '1' hour < uo.charttime
+  and ih.endtime >= uo.charttime
+left join pivoted_bg_art bg
   on  mp.hadm_id = bg.hadm_id
-  and mp.hr = bg.hr
-left join mp_lab lab
+  and ih.endtime - interval '1' hour < bg.charttime
+  and ih.endtime >= bg.charttime
+left join pivoted_lab lab
   on  mp.hadm_id = lab.hadm_id
-  and mp.hr = lab.hr
-order by mp.subject_id, mp.hadm_id, mp.icustay_id, mp.hr;
+  and ih.endtime - interval '1' hour < lab.charttime
+  and ih.endtime >= lab.charttime
+where mp.excluded = 0
+order by mp.subject_id, mp.hadm_id, mp.icustay_id, ih.hr;
